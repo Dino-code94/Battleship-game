62280280d2b4c6b68291c4f29df925773549ee41
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2024-10-20 09:43:37 +0000

    Defining random position

--- run.py
+++ run.py
@@ -13,9 +13,7 @@ SHIP_SIZES = {
     "battleship": 4,
     "shadwell": 3,
     "submarine": 3,
-    "destroyer": 2 
-
-    
+    "destroyer": 2, 
 }
 
 NUM_ROWS = 10
@@ -24,3 +22,20 @@ ROW_IDX = 0
 COL_IDX = 1
 MIN_ROW_LABEL = 'A'
 MAX_ROW_LABEL = 'J'
+
+def get_random_position():
+    """Generates a random location on a board of NUM_ROWS x NUM_COLS."""
+
+    row_choice = chr(
+                    random.choice(
+                        range(
+                            ord(MIN_ROW_LABEL),
+                            ord(MIN_ROW_LABEL) + NUM_ROWS
+                        )
+                    )
+    )
+
+    col_choice = random.randint(0, NUM_COLS - 1)
+
+    return (row_choice, col_choice)
+
