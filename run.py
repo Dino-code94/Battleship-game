ac9675e059eb2b11227117bd04b99f451a9ebd83
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2024-10-20 09:28:52 +0000

    Adding first commands (hit , miss , blank, and creating the game board

---
+++ run.py
@@ -0,0 +1,19 @@
+import random
+
+
+
+HIT_CHAR = 'x'
+MISS_CHAR = 'o'
+BLANK_CHAR = '.'
+VERTICAL = 'v'
+HORIZONTAL = 'h' 
+{
+    
+}
+
+NUM_ROWS = 10
+NUM_COLS = 10
+ROW_IDX = 0
+COL_IDX = 1
+MIN_ROW_LABEL = 'A'
+MAX_ROW_LABEL = 'J'
